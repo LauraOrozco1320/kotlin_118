d9f928ddf8dc93fd38c96e09a4be9b778ca3ec2e
LauraOrozco1320 <orozcolauracamila@gmail.com>
2024-06-13 10:16:21 -0500

    nuevo commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -92,3 +92,15 @@ class RoundTower(
         return super.floorArea() * floors
     }
 }
+
+
+//parte 2
+
+fun main() {
+    println(birthdayGreeting(age = 5))
+    println(birthdayGreeting(age = 2))
+}
+
+fun birthdayGreeting(name: String = "Rover", age: Int): String  {
+    return "Happy Birthday, $name! You are now $age years old!"
+}
